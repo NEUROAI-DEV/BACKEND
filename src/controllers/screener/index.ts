3ec5834bd1fd6a83f6f04aa7132c39b2f84ecceb
misdarmanto <misdarmanto1922@gmail.com>
2026-03-07 21:58:28 +0700

    refactor

--- src/controllers/screener/index.ts
+++ src/controllers/screener/index.ts
@@ -1,11 +1,5 @@
-import { createScreener } from './createScreener'
 import { findAllScreener } from './findAllScreener'
-import { removeScreener } from './removeScreener'
-import { getTopAverages } from './getTopAverages'
 
 export const ScreenerController = {
-  createScreener,
-  findAllScreener,
-  removeScreener,
-  getTopAverages
+  findAllScreener
 }
