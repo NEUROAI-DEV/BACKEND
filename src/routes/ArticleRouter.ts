db892e1429b84bb964e9fe15d64fb1fe7c291595
misdarmanto <misdarmanto1922@gmail.com>
2026-03-18 08:35:17 +0700

    docs(users): add swagger for admin list endpoint
    - Document GET /api/v1/users/admins with pagination and search parameters
    - Align response schema with findAllAdmins controller and UserService.findAllAdmin

--- src/routes/ArticleRouter.ts
+++ src/routes/ArticleRouter.ts
@@ -26,18 +26,21 @@ ArticleRoute.get(
 ArticleRoute.post(
   '/',
   MiddleWares.useAuthorization,
+  MiddleWares.allowAppRoles('admin'),
   MiddleWares.validate({ body: CreateArticleSchema }),
   ArticleController.create
 )
 ArticleRoute.patch(
   '/',
   MiddleWares.useAuthorization,
+  MiddleWares.allowAppRoles('admin'),
   MiddleWares.validate({ body: UpdateArticleSchema }),
   ArticleController.update
 )
 ArticleRoute.delete(
   '/:articleId',
   MiddleWares.useAuthorization,
+  MiddleWares.allowAppRoles('admin'),
   MiddleWares.validate({ params: RemoveArticleSchema }),
   ArticleController.remove
 )
